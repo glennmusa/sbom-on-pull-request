eee9e3ce0b81858e9f6e8e394218857ea9c714a4
Glenn Musa <4622125+glennmusa@users.noreply.github.com>
2022-02-17 19:11:13 +0000

    only generate SBOM if the changes include source changes

--- scratch.sh
+++ scratch.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-last_diff=$(git diff --name-only --pretty="" HEAD HEAD~1)
-echo "$last_diff"
+only_manifest_files=(
+    _manifest/manifest.json
+    _manifest/manifest.json.sha256
+    _manifest/spdx_2.2/manifest.spdx.json
+    _manifest/spdx_2.2/manifest.spdx.json.sha256
+)
+
+last_diff=( $(git diff --name-only --pretty="" HEAD HEAD~1) )
+
+if [[ "${last_diff[*]}" == "${only_manifest_files[*]}" ]]; then
+    echo "These changes are just the manifest files. Exiting code 0."
+    exit 0
+fi
